81b9b09430b0b4e61ac490186396f4c80ecc031f
Caleb Schroder <doomskier@gmail.com>
2019-08-21 12:27:22 -0600

    Thinking this is english class

--- Git.java
+++ Git.java
@@ -4,6 +4,7 @@ public class Git {
 
         System.out.println("\"git is fun!!!!!!!!!!!\"");
         System.out.println("Hi Mom!");
+        System.out.println("The 1900 Hurricane in Galveston killed 6,000 people in Galveston. They never saw it coming. Wait I thought this was AP Lang")
     }
 
 
